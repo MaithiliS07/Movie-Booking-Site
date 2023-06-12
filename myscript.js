d1492ffe1735fe87cdf8f897db837d8a62774a81
Maithili <salunkhemaithili07@gmail.com>
2023-06-12 18:52:38 +0530

    New code added

--- myscript.js
+++ myscript.js
@@ -1,25 +1,9 @@
-//Search box filter code
-/*let page = document.querySelectorAll('body');
-function search(){
-    let searchbox = document.getElementById("search-text").value;
-       //Use innerText if all contents are visible
-    //Use textContent for including hidden elements
-    for (var i = 0; i < page.length; i++) {
-        if(page[i].textContent.toLowerCase()
-                .includes(search_query.toLowerCase())) {
-            page[i].classList.remove("is-hidden");
-        } else {
-            cards[i].classList.add("is-hidden");
-        }
+//Responsive navbar
+function toggle_function() {
+    var x = document.getElementsByTagName("header");
+    if (x.tagName === "header") {
+      x.tagName += " responsive";
+    } else {
+      x.tagName = "header";
     }
-}
-
-//A little delay
-let typingTimer;               
-let typeInterval = 500;  
-let searchInput = document.getElementById('searchbox');
-
-searchInput.addEventListener('keyup', () => {
-    clearTimeout(typingTimer);
-    typingTimer = setTimeout(liveSearch, typeInterval);
-});*/
+  }
