a2697605110197548050086a17b09930eb02d2a5
Maithili <salunkhemaithili07@gmail.com>
2023-07-30 13:37:47 +0530

    New code added

--- myscript.js
+++
@@ -1,9 +0,0 @@
-//Responsive navbar
-function toggle_function() {
-    var x = document.getElementsByTagName("header");
-    if (x.tagName === "header") {
-      x.className += " responsive";
-    } else {
-      x.tagName = "header";
-    }
-  }
